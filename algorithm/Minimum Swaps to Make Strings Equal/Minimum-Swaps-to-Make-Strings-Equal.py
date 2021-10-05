877faaf480838d06eefe8f764bf927d68ca12fe3
Jae Hyeok Jang <jae.jang@payfirma.com>
2021-10-05 07:33:20 -0700

    reduce variables

--- algorithm/Minimum Swaps to Make Strings Equal/Minimum-Swaps-to-Make-Strings-Equal.py
+++ algorithm/Minimum Swaps to Make Strings Equal/Minimum-Swaps-to-Make-Strings-Equal.py
@@ -1,24 +1,22 @@
 class Solution:
     def minimumSwap(self, s1: str, s2: str) -> int:
-        s1_x = s1_y = s2_x = s2_y = count = 0
+        xy = yx =  count = 0
         
         for i in range(len(s1)):
             if s1[i] == s2[i]:
                 continue
             
             if s1[i] == 'x':
-                s1_x += 1
-                s2_y += 1
+                xy += 1
             else:
-                s1_y += 1
-                s2_x += 1
+                yx += 1
 
-        if (s1_x + s2_x) % 2 == 1 or (s1_y + s2_y) % 2 == 1:
+        if (xy + yx) % 2 == 1:
             return -1
         
-        count  = s1_x // 2 + s1_y // 2
+        count  = xy // 2 + yx // 2
         
-        if s1_x % 2 == 1:
+        if xy % 2 == 1:
             count += 2
         
         return count
