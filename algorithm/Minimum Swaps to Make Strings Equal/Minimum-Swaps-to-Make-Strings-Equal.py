d72d5267d7ff9502edeafc1896a606af5ad1d33f
Jae Hyeok Jang <jae.jang@payfirma.com>
2021-10-05 07:27:27 -0700

    reduce memory usage

--- algorithm/Minimum Swaps to Make Strings Equal/Minimum-Swaps-to-Make-Strings-Equal.py
+++ algorithm/Minimum Swaps to Make Strings Equal/Minimum-Swaps-to-Make-Strings-Equal.py
@@ -8,25 +8,17 @@ class Solution:
             
             if s1[i] == 'x':
                 s1_x += 1
+                s2_y += 1
             else:
                 s1_y += 1
-                
-            if s2[i] == 'x':
                 s2_x += 1
-            else:
-                s2_y += 1
-                
+
         if (s1_x + s2_x) % 2 == 1 or (s1_y + s2_y) % 2 == 1:
             return -1
         
-        s1_x_d = s1_x // 2
-        s1_x_m = s1_x % 2
+        count  = s1_x // 2 + s1_y // 2
         
-        s1_y_d = s1_y // 2
-        s1_y_m = s1_y % 2
-        
-        count  = s1_x_d + s1_y_d
-        if s1_x_m == 1:
+        if s1_x % 2 == 1:
             count += 2
         
         return count
