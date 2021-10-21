efc1d028a3638077c163d162afb99d20c5818a3f
Jae Hyeok Jang <jae.jang@payfirma.com>
2021-10-20 21:18:59 -0700

    Valid-Parentheses simpler

--- algorithm/leetcode 100 liked/20. Valid Parentheses/Valid-Parentheses.py
+++ algorithm/leetcode 100 liked/20. Valid Parentheses/Valid-Parentheses.py
@@ -6,23 +6,13 @@ class Solution:
             '}': '{'
         }
         
-        
-        def isOpenning(c): 
-            return True if c == '(' or c == '[' or c == '{' else False
-        
         stack = []
         
         for c in s:
-            if isOpenning(c):
+            if c in parenthese_mapping.values():
                 stack.append(c)
-            else:
-                try:
-                    last = stack.pop()
-                except IndexError:
-                    return False
-                
-                if last != parenthese_mapping[c]:
-                    return False
+            elif stack == [] or stack.pop() != parenthese_mapping[c]:
+                return False
         
-        return True if len(stack) == 0 else False
+        return stack == []
                 
