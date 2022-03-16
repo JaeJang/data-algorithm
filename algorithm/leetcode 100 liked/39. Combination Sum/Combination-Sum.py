529128ff383500c8c72de05e3024629fb3e456bf
Jae Hyeok Jang <jae.jang@payfirma.com>
2022-03-15 18:59:57 -0700

    39. Combination Sum

---
+++ algorithm/leetcode 100 liked/39. Combination Sum/Combination-Sum.py
@@ -0,0 +1,35 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates = sorted(candidates)
+        result = []
+
+        self.helper(candidates, 0, target, [], result)
+        
+        return result;
+    
+    def helper(self, candidates, index, target, currentCom, result):
+        sum = 0;
+        for num in currentCom:
+            sum += num
+
+        sub = target - sum;
+        
+        if sub == 0:
+            result.append(currentCom);
+            return;
+        elif candidates[index] < sub:
+            for i in range(index, len(candidates)):
+                if candidates[i] > sub:
+                    return;
+                else:
+                    tmpCom = currentCom[:]
+                    tmpCom.append(candidates[i])
+                    self.helper(candidates, i, target, tmpCom, result);
+        
+        elif candidates[index] == sub:
+            tmpCom = currentCom[:]
+            tmpCom.append(candidates[index]);
+            result.append(tmpCom);
+            return;
+        else:
+            return;
