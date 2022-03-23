8ed69c3241f7e62dbdc9b1aa5226f375b62f98cd
Jae Hyeok Jang <jae.jang@payfirma.com>
2022-03-22 20:42:27 -0700

    46. Permutations using DFS

--- algorithm/leetcode 100 liked/46. Permutations/Permutations.py
+++ algorithm/leetcode 100 liked/46. Permutations/Permutations.py
@@ -9,3 +9,20 @@ class Solution:
             result = [perm+[num] for perm in result for num in nums if num not in perm];
 
         return result
+
+    
+    # using DFS
+    def permuteV2(self, nums: List[int]) -> List[List[int]]:
+        result = [];
+        
+        self.dfs(nums, [], result);
+        
+        return result;
+        
+    def dfs(self, nums, path, result):
+        if not nums:
+            result.append(path);
+            return;
+        
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path + [nums[i]], result)
