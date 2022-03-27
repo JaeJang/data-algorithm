e705db4fe0d01dd5de7104183310ce2f155ca2d7
Jae Hyeok Jang <jae.jang@payfirma.com>
2022-03-27 11:18:08 -0700

    104. Maximum Depth of Binary Tree

---
+++ algorithm/Graph & Tree/104. Maximum Depth of Binary Tree/Maximum-Depth-of-Binary-Tree.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root, level = 1) {
+    if (!root) return level - 1;
+    
+    return Math.max(maxDepth(root.left, level + 1), maxDepth(root.right, level + 1));
+};
