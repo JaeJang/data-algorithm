556661759f89349f0a230dc44e7e4ae76667b930
Jae Hyeok Jang <jae.jang@payfirma.com>
2022-05-10 16:57:21 -0700

    1143. Longest Common Subsequence

---
+++ algorithm/Dynamic Programming/1143. Longest Common Subsequence/1143. Longest Common Subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+ var longestCommonSubsequence = function(text1, text2) {
+
+    const dp = Array.from({length: text1.length + 1}, () => Array(text2.length+1).fill(0));
+    
+    for (let i = 0; i < text1.length; ++i) {
+        for (let j = 0; j < text2.length; ++j) {
+            if (text1[i] === text2[j]) {
+                dp[i+1][j+1] = dp[i][j] + 1;
+            } else {
+                dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+    }
+    return dp[text1.length][text2.length];
+};
