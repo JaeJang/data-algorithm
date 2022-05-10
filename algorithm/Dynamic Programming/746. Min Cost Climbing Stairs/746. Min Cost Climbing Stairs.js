19412598c01e9ba503850b1e7e9b21b5fce405f8
Jae Hyeok Jang <jae.jang@payfirma.com>
2022-05-09 20:09:55 -0700

    746. Min Cost Climbing Stairs

---
+++ algorithm/Dynamic Programming/746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+ var minCostClimbingStairs = function(cost) {
+    for (let i = 2; i < cost.length; ++i) {
+        cost[i] = cost[i] + Math.min(cost[i - 2], cost[i - 1])
+    }
+    
+    return Math.min(cost[cost.length-2], cost[cost.length-1])
+};
+
+/*
+[1,100,1,1,1,100,1,1,100,1]
+
+[1,100,2,3,3,103,4,5,104,6]
+*/
